33e0a03c896c1a9419724711382607db3f91b7b2
SentryXSI <34962312+SentryXSI@users.noreply.github.com>
2019-09-26 14:39:57 +1000

    minor updates

--- app/components/about/controllers/UsController.php
+++ app/components/about/controllers/UsController.php
@@ -5,18 +5,37 @@ namespace App\Components\About\Controllers;
 
 use App\Kernel\BaseController;
 
+/**-----------------------------------------------------------------------------
+ *
+ * Class UsController
+ *
+ * -----------------------------------------------------------------------------
+ *
+ * @package App\Components\About\Controllers
+ */
 class UsController extends BaseController
 {
+    /**
+     * UsController constructor.
+     *
+     * @param array $response
+     */
     public function __construct( $response = [] ){
         parent::__construct( $response );
     }
 
+    /**
+     * @return mixed|void
+     * @throws \Exception
+     */
     public function getIndex(){
         $this->display();
     }
 
-    public function getTerms()
-    {
+    /**
+     * @throws \Exception
+     */
+    public function getTerms(){
         $this->display();
     }
 }
